addaea38be64c05abb81465677345762e6de6cab
Alin Manole <alin.manole@yahoo.com>
2021-10-26 20:37:34 +0200

    update

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -7,8 +7,8 @@ class TeamInfo extends React.Component {
                 <div className="col-3 border" style={{margin: "20px ", padding: "10px" , boxShadow: "3px 3px 20px 3px #cccccc"}}>
                     <img className="w-25" src={this.props.image} alt="logo" />
                     <p style={{fontWeight:"bold"}}> {this.props.shortName} </p>
-                    <p> Stadium :{this.props.stade}</p>
-                    <p> Address :{this.props.address}</p>
+                    <p> Stadium : {this.props.stade}</p>
+                    <p> Address : {this.props.address}</p>
                     <p> Email : {this.props.email}</p>
                     <p> Phone : {this.props.tel}</p>
                     <p>Creation : {this.props.founded}</p>
