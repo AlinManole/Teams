74226fe5d9c74aab18685526817f9ee4785edc6f
Alin Manole <alin.manole@yahoo.com>
2021-10-26 20:24:59 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import teams from './teams.json'
+import TeamInfo from './components/TeamInfo'
+// import Counter from './components/Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      team: 1
+    }
+  }
+
+  // handleClickUp = () => {
+  //   this.setState({ team: this.state.team + 1 })
+  // }
+
+  // handleClickDown = () => {
+  //   if (this.state.team > 0) {
+  //     this.setState({ team: this.state.team - 1 })
+  //   }
+  // }
+
+
+
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Teams</h1>
+        <div className="row p-2 justify-content-around">
+          {teams.map(team =>
+            <TeamInfo shortName={team.shortName} image={team.crestUrl} stade={team.venue} address={team.address} email={team.email} tel={team.phone} founded={team.founded} />
+          )}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/TeamInfo.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+class TeamInfo extends React.Component {
+    render() {
+        return (
+            <>
+                <div className="col-4 border">
+                    <img className="w-25" src={this.props.image} alt="logo" />
+                    <p> {this.props.shortName} </p>
+                    <p> Stadium :{this.props.stade}</p>
+                    <p> Address :{this.props.address}</p>
+                    <p> Email : {this.props.email}</p>
+                    <p> Phone : {this.props.tel}</p>
+                    <p>Creation : {this.props.founded}</p>
+
+                </div>
+            </>
+        )
+    }
+}
+
+export default TeamInfo
